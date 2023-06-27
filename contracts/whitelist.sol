6dde0f49669c97820e0f37717117fccaaa1601ac
fc56283 <fc56283@alunos.fc.ul.pt>
2023-06-28 00:06:49 +0100

    whitlist contract created and gitignore added

---
+++ contracts/whitelist.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+
+contract whitelist {
+
+    uint8 public maxUsers;
+    uint8 public currentUsers;
+    
+    mapping (address => bool) public _whitelist;
+
+    constructor(uint8 _maxUsers) {
+        maxUsers = _maxUsers;
+    }
+
+    function add(address user) public {
+
+        require(!_whitelist[msg.sender], "Sender has already been whitelisted");
+
+        require(currentUsers < maxUsers, "No more users can be whitelisted"); 
+
+        _whitelist[user] = true;
+        currentUsers++;
+        
+    }
+
+    function remove(address user) public {
+    
+
+        if (_whitelist[user] = true)
+            currentUsers--;
+    
+        delete _whitelist[user];
+    
+    }
+
+    //add
+    //remove
+    //checkIfin
+
+}
